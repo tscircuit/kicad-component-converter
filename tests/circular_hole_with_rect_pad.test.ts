8020b0fb8e8961c6b161b455afaf1117cf99d81a
Rishabh Gupta <38923768+imrishabh18@users.noreply.github.com>
2025-09-10 22:59:45 +0530

    Add support for `circular_hole_with_rect_pad` (#129)
    * WIP
    
    * support the shape
    
    * add asset

---
+++ tests/circular_hole_with_rect_pad.test.ts
@@ -0,0 +1,18 @@
+import { test, expect } from "bun:test"
+import { parseKicadModToCircuitJson } from "src"
+import { convertCircuitJsonToPcbSvg } from "circuit-to-svg"
+import fs from "node:fs"
+import { join } from "path"
+
+test("circular_hole_with_rect_pad.kicad_mod", async () => {
+  const fixturePath = join(
+    import.meta.dirname,
+    "data/circular_hole_with_rect_pad.kicad_mod",
+  )
+  const fileContent = fs.readFileSync(fixturePath).toString()
+
+  const circuitJson = await parseKicadModToCircuitJson(fileContent)
+  Bun.write("circuit-json.json", JSON.stringify(circuitJson, null, 2))
+  const result = convertCircuitJsonToPcbSvg(circuitJson as any)
+  expect(result).toMatchSvgSnapshot(import.meta.path)
+})
