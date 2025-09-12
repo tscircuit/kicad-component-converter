8b1f30ae8c87b6281701d780eb8fdbee13f66d81
seveibar <seveibar@gmail.com>
2025-09-11 20:53:45 -0700

    repro test init

---
+++ tests/repros/jst-loading.test.ts
@@ -0,0 +1,6 @@
+import { test, expect } from "bun:test"
+
+// TODO load the following file and take a visual snapshot
+// and log the pcb_plated_holes
+// Connector_JST.pretty/JST_PH_B2B-PH-K_1x02_P2.00mm_Vertical.kicad_mod
+test("jst loading issue with pin numbers", () => {})
