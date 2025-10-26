122cc7791bbc59c284478169d04ee276ff2d616f
Exceluyi <ademiluyiexcellence@gmail.com>
2025-10-26 12:38:22 +0100

    Migrate to bun testing

--- tests/repros/repro159-fp_poly.test.ts
+++ tests/repros/repro159-fp_poly.test.ts
@@ -1,12 +1,12 @@
-import test from "ava"
+import { test, expect } from "bun:test"
 import { parseKicadModToKicadJson } from "src/parse-kicad-mod-to-kicad-json"
 import { convertKicadJsonToTsCircuitSoup } from "src/convert-kicad-json-to-tscircuit-soup"
 import { readFileSync } from "fs"
 import { join } from "path"
 
-test("repro159 fp_poly with arc", async (t) => {
-  const fileContent = readFileSync(join(__dirname, "../data/repro159-fp_poly.kicad_mod"), "utf8")
+test("repro159 fp_poly with arc", async () => {
+  const fileContent = readFileSync("tests/data/repro159-fp_poly.kicad_mod", "utf8")
   const kicadJson = parseKicadModToKicadJson(fileContent)
   const soup = await convertKicadJsonToTsCircuitSoup(kicadJson)
-  t.snapshot(soup)
+  expect(soup).toMatchSnapshot()
 })
